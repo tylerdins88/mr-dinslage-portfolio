78dedbde4a8db55b692c8f0d4ef2130d75db480b
Tyler <tyler.dins88@gmail.com>
2024-04-29 19:54:09 -0700

    vite.config chagne

--- dinsport/vite.config.js
+++ dinsport/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/mr-dinslage-portfolio/',
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
